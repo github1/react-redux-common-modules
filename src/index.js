a5641b3cc5febabfa9d2839445e3712dc998c472
github1 <john.collier4@gmail.com>
2019-04-26 00:14:23 -0400

    update react-redux-common-modules example
    ref-a935aa3e965b1ee038b90eabf85fcea40bbfef9a

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Alerts, {displayAlert} from './alerts';
+import Alerts, {displayAlert, hideAllAlerts, requestConfirmation} from './alerts';
 import {Alerts as AlertsContainer} from './alerts/components/alert';
 import {Module} from '@github1/redux-modules';
 import {Provider} from 'react-redux'
-import 'bootstrap/scss/bootstrap.scss';
+import './index.scss';
 
 const store = Module.createStore(Alerts);
 
 store.dispatch(displayAlert({
     title: 'hi',
-    message: 'asdas'
+    message: 'asdas',
+    timeout: 5000
+}));
+
+store.dispatch(requestConfirmation({
+    title: 'Hello',
+    message: 'Test message'
 }));
 
 const root = document.createElement('div');
 document.body.appendChild(root);
 
-ReactDOM.render(<Provider store={store}><AlertsContainer/></Provider>, root);
+ReactDOM.render(<Provider store={store}>
+    <div>
+        <button onClick={ () => { store.dispatch(hideAllAlerts()) } }>Hide
+            Alerts
+        </button>
+        <AlertsContainer/>
+    </div>
+</Provider>, root);
 
