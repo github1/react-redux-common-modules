5feb273b07f7c08b143180b0eed08e3cebc810fb
github1 <john.collier4@gmail.com>
2019-08-03 10:53:22 -0400

    auto open browser on build-tools devserver task
    ref-df249587c0599c0da60bcb16b46ce3c56e53df25

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Alerts, {displayAlert} from './alerts';
+import {Alerts as AlertsContainer} from './alerts/components/alert';
+import {Module} from '@common/redux-modules';
+import {Provider} from 'react-redux'
+import 'bootstrap/scss/bootstrap.scss';
+
+const store = Module.createStore(Alerts);
+
+store.dispatch(displayAlert({
+    title: 'hi',
+    message: 'asdas'
+}));
+
+const root = document.createElement('div');
+document.body.appendChild(root);
+
+ReactDOM.render(<Provider store={store}><AlertsContainer/></Provider>, root);
+
